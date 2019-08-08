5009fdd323bef3f87d267edc0b31a55f9290d264
aratep95 <tepanyan95@gmail.com>
2019-08-08 08:32:15 +0400

    add comments to code

--- src/App.js
+++ src/App.js
@@ -11,21 +11,23 @@ class App extends Component{
     }
 
     componentDidMount() {
-        this.getItems()
+        this.getItems();
     }
 
     getItems = () => {
-        const API = ' https://api.jsonbin.io/b/5b97f370db948c68635f6dbc'
+        const API = ' https://api.jsonbin.io/b/5b97f370db948c68635f6dbc';
 
+        // make a get request to API
         fetch(API)
             .then(response => response.json())
-            .then(data => this.setState({data}))
+            .then(data => this.setState({data})) // put request data into state
             .catch(err => console.log(err))
     }
 
     render() {
         const {data} = this.state
 
+        // if there is no data fetched yet return loading message
         if(data.length <= 0)
             return <div className='loading'>Loading...</div>
 
